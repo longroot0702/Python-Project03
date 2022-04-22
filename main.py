abd72ca08290240954997297046120ac06d36aec
Geunyoung <longroot0702@gmail.com>
2022-04-23 00:58:31 +0900

    File Version 0.3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 - File Name: main.py
 - Writer: Geunyoung kim
-- Update Information: [2022, 04, 22] File Version 0.2
+- Update Information: [2022, 04, 23] File Version 0.3
 '''
 
 import os, re, codecs
@@ -55,7 +55,34 @@ def collectStageDia():
 
 # Collect dialogue included particular word
 def collectDiaPartiWord():
-    pass
+    print("----------------------------------------------\n")
+    print("  [Collect dialogue included particular word]\n")
+    word = input('Please enter a word: ')
+
+    f = codecs.open('friends101.txt', 'r', encoding = 'utf-8')
+    script101 = f.readlines()
+    f.close()
+
+    Lines = []
+
+    for item in script101:
+        if re.match(r'[A-Z][a-z]+:', item):
+            Lines += [item]
+
+    dialogue = ''
+    for item in Lines:
+        if re.search(word, item):
+            dialogue += item
+
+    if len(dialogue) == 0:
+        print('\nError: Dialogue or word is not exist...')
+    else:
+        f = codecs.open(word + '.txt', 'w', encoding = 'utf-8')
+        f.write(dialogue)
+        f.close()
+        print('\nText file is created!')
+    
+    print("----------------------------------------------\n")
 
 while True:
     menu()
@@ -72,7 +99,7 @@ while True:
         elif choice == 3:
             collectStageDia()
         elif choice == 4:
-            collectDiaPartiWord
+            collectDiaPartiWord()
         elif choice == 5:
             exit()
         else:
